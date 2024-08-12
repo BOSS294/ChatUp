6ebe821f0e6415c02df09eb678b1e1987ef2a48e
Mayank Chawdhari <72921622+BOSS294@users.noreply.github.com>
2024-08-13 00:27:10 +0530

    FLOATING NAV BAR CREATED
    ONLY TEXTS
    
    ( CHANGE IT WITH ICONS )

---
+++ Assets/Website/Resources/nav.php
@@ -0,0 +1,15 @@
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=HK+Modular:wght@400;700&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<div class="navbar">
+        <div class="navbar-logo">
+            <h1>ChatUp</h1>
+        </div>
+        <ul class="navbar-menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Account</a></li>
+            <li><a href="#"> Contact</a></li>
+        </ul>
+    </div>
