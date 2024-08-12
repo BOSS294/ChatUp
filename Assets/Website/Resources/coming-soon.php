66d54d5f28ad65d7d4d1093333cedffd9ca0b07d
Mayank Chawdhari <72921622+BOSS294@users.noreply.github.com>
2024-08-13 00:09:30 +0530

    CHATUP WEBAPP INITIATION

---
+++ Assets/Website/Resources/coming-soon.php
@@ -0,0 +1,14 @@
+<div class="coming-soon">
+    <h1>Coming Soon</h1>
+    <p>We're working hard to bring you something amazing. Stay tuned!</p>
+    <div class="button-container">
+        <button class="modern-button">
+            <span class="material-icons">update</span>
+            Check Update
+        </button>
+        <button class="modern-button">
+            <span class="material-icons">favorite</span>
+            Contribute
+        </button>
+    </div>
+</div>
